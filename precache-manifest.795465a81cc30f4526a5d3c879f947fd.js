693fb60d7efe418880e9525b470a7b601979409a
jeliel <jeliel.augusto10@gmail.com>
2019-09-25 21:11:05 -0400

    Updates

---
+++ precache-manifest.795465a81cc30f4526a5d3c879f947fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4aa00978a36ee1429cc490474fb7d1",
+    "url": "/primeiro-react-monsters/index.html"
+  },
+  {
+    "revision": "6f0e92a1ed90a38da794",
+    "url": "/primeiro-react-monsters/static/css/main.8e368f63.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/primeiro-react-monsters/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "6f0e92a1ed90a38da794",
+    "url": "/primeiro-react-monsters/static/js/main.42b5a273.chunk.js"
+  },
+  {
+    "revision": "107d631fffe066b33008",
+    "url": "/primeiro-react-monsters/static/js/runtime-main.31c387fd.js"
+  }
+]);
